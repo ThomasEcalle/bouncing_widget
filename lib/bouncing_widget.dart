4129ca6207d0cb36319f289aa7748afbcc145831
Ecalle Thomas <thomasecalle@hotmail.fr>
2019-11-07 00:56:24 +0100

    Add comments

--- lib/bouncing_widget.dart
+++ lib/bouncing_widget.dart
@@ -3,9 +3,13 @@ library bouncing_widget;
 import 'package:flutter/material.dart';
 
 class BouncingWidget extends StatefulWidget {
+  /// Child that will receive the bouncing animation
   final Widget child;
+
+  /// Callback on click event
   final VoidCallback onPressed;
 
+  /// BouncingWidget constructor
   const BouncingWidget({
     Key key,
     @required this.child,
@@ -17,21 +21,33 @@ class BouncingWidget extends StatefulWidget {
 }
 
 class _BouncingWidgetState extends State<BouncingWidget> with SingleTickerProviderStateMixin {
+  //// Animation controller
   AnimationController _controller;
+
+  /// View scale used in order to make the bouncing animation
   double _scale;
+
+  /// Key of the given child used to get its size and position whenever we need
   GlobalKey _childKey = GlobalKey();
+
+  /// If the touch position is outside or not of the given child
   bool _isOutside = false;
 
+  /// Simple getter on widget's child
   Widget get child => widget.child;
 
+  /// Simple getter on widget's onPressed callback
   VoidCallback get onPressed => widget.onPressed;
 
+  /// We instantiate the animation controller
+  /// The idea is to call setState() each time the controller's
+  /// value changes
   @override
   void initState() {
     _controller = AnimationController(
       vsync: this,
       duration: Duration(
-        milliseconds: 100,
+        milliseconds: 200,
       ),
       lowerBound: 0.0,
       upperBound: 0.1,
@@ -41,6 +57,9 @@ class _BouncingWidgetState extends State<BouncingWidget> with SingleTickerProvid
     super.initState();
   }
 
+  /// Each time the [_controller]'s value changes, build() will be called
+  /// We just have to calculate the appropriate scale from the controller value
+  /// and pass it to our Transform.scale widget
   @override
   Widget build(BuildContext context) {
     _scale = 1 - _controller.value;
@@ -60,26 +79,33 @@ class _BouncingWidgetState extends State<BouncingWidget> with SingleTickerProvid
     );
   }
 
+  /// Simple method called when we need to notify the user of a press event
   _triggerOnPressed() {
     if (onPressed != null) {
       onPressed();
     }
   }
 
+  /// We start the animation
   _onTapDown(TapDownDetails details) {
     _controller.forward();
   }
 
+  /// We reverse the animation and notify the user of a press event
   _onTapUp(TapUpDetails details) {
     _controller.reverse();
     _triggerOnPressed();
   }
 
+  /// Here we are listening on each change when drag event is triggered
+  /// We must keep the [_isOutside] value updated in order to use it later
   _onDragUpdate(DragUpdateDetails details, BuildContext context) {
     final Offset touchPosition = details.globalPosition;
     _isOutside = _isOutsideChildBox(touchPosition);
   }
 
+  /// When this callback is triggered, we reverse the animation
+  /// If the touch position is inside the children renderBox, we notify the user of a press event
   _onLongPressEnd(LongPressEndDetails details, BuildContext context) {
     final Offset touchPosition = details.globalPosition;
 
@@ -90,6 +116,9 @@ class _BouncingWidgetState extends State<BouncingWidget> with SingleTickerProvid
     _controller.reverse();
   }
 
+  /// When this callback is triggered, we reverse the animation
+  /// As we do not have position details, we use the [_isOutside] field to know
+  /// if we need to notify the user of a press event
   _onDragEnd(DragEndDetails details) {
     if (!_isOutside) {
       _triggerOnPressed();
@@ -97,6 +126,8 @@ class _BouncingWidgetState extends State<BouncingWidget> with SingleTickerProvid
     _controller.reverse();
   }
 
+  /// Method called when we need to now if a specific touch position is inside the given
+  /// child render box
   bool _isOutsideChildBox(Offset touchPosition) {
     final RenderBox childRenderBox = _childKey.currentContext.findRenderObject();
     final Size childSize = childRenderBox.size;
